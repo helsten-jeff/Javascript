cb59e07e418e5fa959ff34224f755fd927edbed6
helsten-jeff <55448496+helsten-jeff@users.noreply.github.com>
2019-09-19 09:03:01 -0600

    Update objects-inheritance.js
    add instantiation example

--- examples/objects-inheritance.js
+++ examples/objects-inheritance.js
@@ -1,4 +1,4 @@
- /** OBJECT CREATION **/
+ /** OBJECT CREATION */
 
 const user = {
   id: 343434343,
@@ -38,86 +38,35 @@ console.log("newUser", newUser);
 //   }
 
 
-/** INHERITANCE **/
+/* Object Properties */
 
-const arr = []
-console.log(arr.__proto__)
-// outputs methods on prototype inherit from Array()
+//You can key off of object properties in a few ways in javascript:
+const themeChoice = user.preferences.theme
+console.log(themeChoice)
+// 'dark'
 
-const obj = []
-console.log(obj.__proto__)
-// outputs methods on prototype inherit from Object()
+const userPrefs = user['preferences']
+console.log(userPrefs)
+// Object {
+//   subscribed: true,
+//   theme: dark
+// }
 
+/* Object methods are properties that contain functions */
 
-function Actor(name, role){
-  this.name = name
-  this.role = role
+// Strings contain inherit methods
+const string = 'this is a cool string'
+console.log(string.includes('cool'))
+// true
+
+// Let's make a method on our user that returns their full name
+user.fullName = function() {
+  return this.firstName + ' ' + this.lastName
 }
+console.log(user.fullName())
+// "Billy Goat"
 
-function Fighter(name, weapon){
-  // chain constructor
-  Actor.call(this, name, 'fighter')
-  // new Fighter property 
-  this.weapon = weapon
-}
-
-
-// Copy prototypes
-Fighter.prototype = Object.create(Actor.prototype);
-
-// Add method to Actor prototype
-Actor.prototype.sayHello = function () {
-  return `The ${this.role} ${this.name} says 'greetings!'.`;
-}
-// Add method to Fighter prototype
-Fighter.prototype.lunge = function(){
-  return `${this.name} lunged forward with his ${this.weapon}!`
-}
-
-let minstrel = new Actor('Freddy', 'minstrel')
-console.log(minstrel.sayHello())
-// "The minstrel Freddy says 'greetings!'."
-
-const philip = new Fighter('Philip', 'sword')/** OBJECT CREATION **/
-
-const user = {
-  id: 343434343,
-  firstName: "Billy",
-  lastName: "Goat",
-  preferences: { theme: "dark", subscribed: true }
-};
-
-// The example above is more performant than:
-const newUser = new Object();
-newUser.firstName = "Billy";
-newUser.lastName = "Goat";
-newUser.id = 343434343;
-newUser.preferences = new Object();
-newUser.preferences.theme = "dark";
-newUser.preferences.subscribed = true;
-
-console.log("user", user);
-//   "user" Object {
-//     firstName: "Billy",
-//     id: 343434343,
-//     lastName: "Goat",
-//     preferences: Object {
-//       subscribed: true,
-//       theme: "dark"
-//     }
-//   }
-console.log("newUser", newUser);
-//   "newUser" Object {
-//     firstName: "Billy",
-//     id: 343434343,
-//     lastName: "Goat",
-//     preferences: Object {
-//       subscribed: true,
-//       theme: "dark"
-//     }
-//   }
-
-/** INHERITANCE **/
+/** INHERITANCE AND INSTANTIATION **/
 
 const arr = [];
 console.log(arr.__proto__);
